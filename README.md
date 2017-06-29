42b61972af13f3a1bc20da46355404bd74f09abb
John Pruitt <jgpruitt@gmail.com>
2017-06-29 18:14:13 -0500

    comments etc.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # nilif
 
+A Go library to essentially provide a type-safe (for built-in types) way to test variables for a given value and substitute with nil if matched akin to SQL's nullif function.
+
+This is a pretty trivial thing, but here it is anyway.
 
--- nilif.go
+++ nilif.go
@@ -22,8 +22,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+// Package nilif provides a type-safe way to test variables for a given value and substitute with nil if matched akin to SQL's nullif function.
 package nilif
 
+// Bool returns nil if arg equals val, otherwise it returns a pointer to arg
 func Bool(arg bool, val bool) *bool {
 	if arg == val {
 		return nil
@@ -31,6 +33,7 @@ func Bool(arg bool, val bool) *bool {
 	return &arg
 }
 
+// Rune returns nil if arg equals val, otherwise it returns a pointer to arg
 func Rune(arg rune, val rune) *rune {
 	if arg == val {
 		return nil
@@ -38,6 +41,7 @@ func Rune(arg rune, val rune) *rune {
 	return &arg
 }
 
+// String returns nil if arg equals val, otherwise it returns a pointer to arg
 func String(arg string, val string) *string {
 	if arg == val {
 		return nil
@@ -45,6 +49,7 @@ func String(arg string, val string) *string {
 	return &arg
 }
 
+// Int returns nil if arg equals val, otherwise it returns a pointer to arg
 func Int(arg int, val int) *int {
 	if arg == val {
 		return nil
@@ -52,6 +57,7 @@ func Int(arg int, val int) *int {
 	return &arg
 }
 
+// Int8 returns nil if arg equals val, otherwise it returns a pointer to arg
 func Int8(arg int8, val int8) *int8 {
 	if arg == val {
 		return nil
@@ -59,6 +65,7 @@ func Int8(arg int8, val int8) *int8 {
 	return &arg
 }
 
+// Int16 returns nil if arg equals val, otherwise it returns a pointer to arg
 func Int16(arg int16, val int16) *int16 {
 	if arg == val {
 		return nil
@@ -66,6 +73,7 @@ func Int16(arg int16, val int16) *int16 {
 	return &arg
 }
 
+// Int32 returns nil if arg equals val, otherwise it returns a pointer to arg
 func Int32(arg int32, val int32) *int32 {
 	if arg == val {
 		return nil
@@ -73,6 +81,7 @@ func Int32(arg int32, val int32) *int32 {
 	return &arg
 }
 
+// Int64 returns nil if arg equals val, otherwise it returns a pointer to arg
 func Int64(arg int64, val int64) *int64 {
 	if arg == val {
 		return nil
@@ -80,6 +89,7 @@ func Int64(arg int64, val int64) *int64 {
 	return &arg
 }
 
+// Uint returns nil if arg equals val, otherwise it returns a pointer to arg
 func Uint(arg uint, val uint) *uint {
 	if arg == val {
 		return nil
@@ -87,6 +97,7 @@ func Uint(arg uint, val uint) *uint {
 	return &arg
 }
 
+// Uint8 returns nil if arg equals val, otherwise it returns a pointer to arg
 func Uint8(arg uint8, val uint8) *uint8 {
 	if arg == val {
 		return nil
@@ -94,6 +105,7 @@ func Uint8(arg uint8, val uint8) *uint8 {
 	return &arg
 }
 
+// Uint16 returns nil if arg equals val, otherwise it returns a pointer to arg
 func Uint16(arg uint16, val uint16) *uint16 {
 	if arg == val {
 		return nil
@@ -101,6 +113,7 @@ func Uint16(arg uint16, val uint16) *uint16 {
 	return &arg
 }
 
+// Uint32 returns nil if arg equals val, otherwise it returns a pointer to arg
 func Uint32(arg uint32, val uint32) *uint32 {
 	if arg == val {
 		return nil
@@ -108,6 +121,7 @@ func Uint32(arg uint32, val uint32) *uint32 {
 	return &arg
 }
 
+// Uint64 returns nil if arg equals val, otherwise it returns a pointer to arg
 func Uint64(arg uint64, val uint64) *uint64 {
 	if arg == val {
 		return nil
@@ -115,6 +129,7 @@ func Uint64(arg uint64, val uint64) *uint64 {
 	return &arg
 }
 
+// Float32 returns nil if arg equals val, otherwise it returns a pointer to arg
 func Float32(arg float32, val float32) *float32 {
 	if arg == val {
 		return nil
@@ -122,6 +137,7 @@ func Float32(arg float32, val float32) *float32 {
 	return &arg
 }
 
+// Float64 returns nil if arg equals val, otherwise it returns a pointer to arg
 func Float64(arg float64, val float64) *float64 {
 	if arg == val {
 		return nil
